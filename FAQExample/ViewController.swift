a9b50ab134ddd08aa112b19d7430b8efbde2f220
amul <amul@administrators-mac-mini.local>
2017-12-29 11:43:40 +0530

    Initial Commit

---
+++ FAQExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FAQExample
+//
+//  Created by amul on 29/12/17.
+//  Copyright © 2017 amul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
